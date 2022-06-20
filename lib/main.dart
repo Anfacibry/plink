78fd071bc51e176a30cea78ad0251868120e3d2b
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-06-20 15:14:48 -0300

    atualizando main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'screens/tela_login.dart';
+import 'package:flutter/services.dart';
+import 'package:plink/themes/models/cores.dart';
+
+import 'screens/telaLogin/tela_login.dart';
+import 'themes/models/temas.dart';
 
 void main() => runApp(const Plink());
 
@@ -9,9 +13,21 @@ class Plink extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePlink(),
+      theme: ThemeData.light().copyWith(extensions: <ThemeExtension<dynamic>>[
+        TemaCustom(
+            cor: Cores.corSecundaria,
+            decoration: const BoxDecoration(),
+            buttonStyle: ElevatedButton.styleFrom(
+              primary: Cores.corSecundaria,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            )),
+      ]),
+      home: const HomePlink(),
     );
   }
 }
