4a8e7844a35d50640b7fac76c2e1c6b98a339b03
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-07-18 14:14:25 -0300

    adicionando uma cor para thema

--- lib/themes/models/cores.dart
+++ lib/themes/models/cores.dart
@@ -3,12 +3,13 @@ import 'package:flutter/cupertino.dart';
 class Cores {
   final BuildContext context;
   static const Color corPrimaria = Color(0xFFFEF9F9);
-  static const Color corSecundaria = Color(0xFFFF5757);
-  static const Color corBotao = Color(0xFFB9B9B9);
+  static const Color corBotao = Color(0xFFFF5757);
+  static const Color corBotaoApagadoBodyText1 = Color(0xFFB9B9B9);
   static const Color corTextoBotao = Color(0xFFFFFFFF);
-  static const Color corIconeETextoSecundario = Color(0xFF838383);
+  static const Color corIconeESubtitle1 = Color(0xFF838383);
   static const Color corContainer = Color(0xFFFFDDA8);
-  static const Color corTextoPrincipal = Color(0xFF606060);
+  static const Color corheadline5 = Color(0xFF606060);
+  static const Color corCaixaTexto = Color(0xFFF7F7F7);
 
   Cores({required this.context});
   double get altura => MediaQuery.of(context).size.height;
