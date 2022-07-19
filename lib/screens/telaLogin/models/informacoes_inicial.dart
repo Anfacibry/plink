22f02507c0419215dbedbf0924367a22a914bafb
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-07-19 10:50:30 -0300

    atualizando import

--- lib/screens/telaLogin/models/informacoes_inicial.dart
+++
@@ -1,7 +0,0 @@
-class InformacoesInicias {
-  static const List<String> informacoes = [
-    "Seu jeito único de pedir comida e poder ver as melhores delícias com os melhores preços",
-    "Todos nossos pedidos são feitos com muito amor e carinho para que você sinta o sabor da felicidade e torne seu dia mais feliz",
-    "Temos um excelente atendimento e estamos ansiosos para lhe atender.",
-  ];
-}
